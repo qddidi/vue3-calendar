ceb932559330731be6d4542b33acc4125acf76a1
张迪生 <2046353594@qq.com>
2023-03-08 16:03:21 +0800

    feat: init

--- src/http/home/index.ts
+++ src/http/home/index.ts
@@ -1,83 +1,13 @@
-import $http from '../axios'
+import $http from "../axios";
 //所有商品类目
 const cataloglist = (data: any) => {
-    return $http({
-        url: '/api/publish_catalog/catalog_list',
-        method: 'get',
-        data
-    })
-}
-//所有商品列表
-const publishinfolist = (data: any) => {
-    return $http({
-        url: '/api/publish_info/publish_info_list',
-        method: 'post',
-        data
-    })
-}
-//详情
-const publishinfodetail = (data: any) => {
-    return $http({
-        url: '/api/publish_info/publish_info_detail',
-        method: 'post',
-        data
-    })
-}
+  return $http({
+    url: "/api/publish_catalog/catalog_list",
+    method: "get",
+    data,
+  });
+};
 
-const replyinfolist = (data: any) => {
-    return $http({
-        url: '/api/publish_reply/reply_info_list',
-        method: 'post',
-        data
-    })
-}
-//发布信息
-
-const savepublishinfo = (data: any) => {
-    return $http({
-        url: '/api/publish_info/save_publish_info',
-        method: 'post',
-        data
-    })
-}
-//提交评论
-const savereplyinfo = (data: any) => {
-    return $http({
-        url: '/api/publish_reply/save_reply_info',
-        method: 'post',
-        data
-    })
-}
-//
-const login = (data: any) => {
-    return $http({
-        url: '/api/login',
-        method: 'post',
-        data
-    })
-}
-const complainttag = (data: any) => {
-    return $http({
-        url: '/api/complaint_tag/list',
-        method: 'get',
-        data
-    })
-}
-const tocomplaint = (data: any) => {
-    return $http({
-        url: '/api/complaint/to_complaint',
-        method: 'post',
-        data
-    })
-}
 export default {
-    cataloglist,
-    publishinfolist,
-    publishinfodetail,
-    replyinfolist,
-    savereplyinfo,
-    savepublishinfo,
-    login,
-    complainttag,
-    tocomplaint
-}
+  cataloglist,
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,26 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "Home" */ '../view/Home.vue'),
-        meta: {
-            keepAlive: true,
-        }
+  {
+    path: "/",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "Home" */ "../view/Home.vue"),
+    meta: {
+      keepAlive: true,
     },
-    {
-        path: '/detail',
-        name: 'detail',
-        component: () => import(/* webpackChunkName: "Home" */ '../view/detial/index.vue')
-    },
-    {
-        path: '/push',
-        name: 'push',
-        component: () => import(/* webpackChunkName: "push" */ '../view/push/index.vue')
-    }
-]
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    scrollBehavior(to, from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition
-        } else {
-            return { top: 0 }
-        }
-    },
-    routes
-})
+  history: createWebHashHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+  routes,
+});
 
-export default router
+export default router;
